9057dd48c199faba810ebb3ff909c2983ec122a0
sanjita <sanjitakandel643@gmail.com>
2026-04-12 18:10:03 +0545

    Fetched API

--- src/Component/Flashsale.jsx
+++ src/Component/Flashsale.jsx
@@ -1,13 +1,70 @@
 import React, { useState } from "react";
 import NavBar from "../Navbar/NavBar";
+import { useEffect } from "react";
 import "./flashSale.css"
+import axios from "axios";
 import { FaAngleDown } from "react-icons/fa";
 function Flashsale() {
-  const[open,setOpen] = useState(false)
+  const[show,setShow]=useState([]);
+  const[category,setCategory]=useState("all");
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products?limit=0")
+
+      .then((response) => {
+        setShow(response.data.products);
+      })
+      .catch((error) => {
+        console.error("error fetching posts", error);
+      });
+  
+  }, [])
+   const filterCategory=    category==="all" ?show :show.filter((item)=>item.category ===category)
+  
   return (
     <div>
       <NavBar />
-     
+      <header>
+        <h1
+          style={{
+            fontSize: "1.1rem",
+            marginLeft: "5rem",
+            marginTop: "0.5rem",
+          }}
+        >
+          categories
+        </h1>
+        <select
+          style={{ marginLeft: "5rem" }}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="beauty">Beauty</option>
+          <option value="fragrances">Fragrances</option>
+          <option value="furniture">Furniture</option>
+          <option value="groceries">Groceries</option>
+          <option value="sports-accessories">Sports-accessories</option>
+        </select>
+      </header>
+      <section>
+        <div className="Apicategories">
+          {filterCategory.length > 0 ? (
+            filterCategory.map((photos) => (
+              <div key={photos.id} className="image_Containers">
+                <img className="apiImage" src={photos.images[0]} alt="" />
+
+                <h5>{photos.title}</h5>
+                <br />
+                <p>Category : {photos.category}</p>
+                <b> Price : ${photos.price}</b>
+              </div>
+            ))
+          ) : (
+            <p>no found</p>
+          )}
+        </div>
+      </section>
+      
     </div>
   );
 }
--- src/Navbar/NavBar.jsx
+++ src/Navbar/NavBar.jsx
@@ -3,24 +3,41 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 function NavBar() {
   const [open, setOpen] = useState(false);
-  const[openQr,setopenQr]=useState(false)
+  const[openQr,setopenQr]=useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+ 
   return (
     <div className="container">
       <div className="div">
         <nav className="Nav-Bar">
           <Link to="/"></Link>
           <div className="Down">
-            
-            <a onClick={()=>{
-              setopenQr(!openQr)
-            }}
-             
+            <a
+              onClick={() => {
+                setopenQr(!openQr);
+              }}
             >
-            
               SAVE MORE ON APP
             </a>
-           {openQr&&( <div className="Down-Drop"></div>)}
-            
+            {openQr && (
+              <div className="Down-Drop">
+                <p
+                  style={{
+                    fontSize: "0.8rem",
+                    marginLeft: "2rem",
+                    marginTop: "1rem",
+                  }}
+                >
+                  Download the App
+                </p>
+                <img
+                  style={{ marginLeft: "1.6rem", marginTop: "1rem" }}
+                  src="https://gw.alicdn.com/imgextra/i2/O1CN01da6D0K1Khogk708ZP_!!6000000001196-0-tps-150-150.jpg"
+                  alt=""
+                />
+                <a href="https://apps.apple.com/us/app/daraz-online-shopping-app/id978058048"></a>
+              </div>
+            )}
           </div>
           <Link to="/seller">BECOME A SELLER</Link>
           <div className="dropdown-container">
@@ -43,8 +60,24 @@ function NavBar() {
             )}
           </div>
 
-          <Link to="/login">LOGIN</Link>
-          <Link to="/signup">SIGN UP</Link>
+          <a onClick={() => setShowLogin(true)}>LOGIN</a>
+          {showLogin && (
+            <div className="modal-overlay">
+              <div className="modal-box">
+                <span className="close" onClick={() => setShowLogin(false)}>
+                  ✖
+                </span>
+
+                <h2>Password | Phone Number</h2>
+
+                <input type="text" placeholder="Email / Phone" required />
+                <input type="password" placeholder="Password" required />
+
+                <button>LOGIN</button>
+              </div>
+            </div>
+          )}
+          <a>SIGN UP</a>
         </nav>
       </div>
 
@@ -59,7 +92,7 @@ function NavBar() {
           <input type="text" placeholder="search in Daraz" />
         </div>
         <div className="div3">
-          <i class="fa-solid fa-cart-shopping"></i>
+          <i className="fa-solid fa-cart-shopping"></i>
         </div>
       </div>
     </div>
